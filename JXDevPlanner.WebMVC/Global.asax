10ae27e927dc35b02bbc556c9764b15aa2af4160
JHNUXER <johnhoganuke2@gmail.com>
2019-03-05 10:22:25 -0500

    Add project files.

---
+++ JXDevPlanner.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JXDevPlanner.WebMVC.MvcApplication" Language="C#" %>
---
+++ JXDevPlanner.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JXDevPlanner.WebMVC.Startup))]
+namespace JXDevPlanner.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
