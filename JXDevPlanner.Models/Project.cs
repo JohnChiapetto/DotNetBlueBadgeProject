eb74bf1411c2cc3f2ad89a38af8f579d40c61995
JHNUXER <johnhoganuke2@gmail.com>
2019-03-07 10:15:26 -0500

    Initial Commit

---
+++ JXDevPlanner.Models/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXDevPlanner.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid ProjectID { get; set; }
+        public Guid Creator { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public DateTimeOffset DateCreatedUTC { get; set; }
+        public DateTimeOffset DateModifiedUTC { get; set; }
+
+        class ProjectDbContext : <Project> {
+        }
+    }
+}
---
+++ JXDevPlanner.Models/ProjectListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXDevPlanner.Models
+{
+    public class ProjectListItem
+    {
+        public ulong ProjectID { get; set; }
+        public string Name { get; set; }
+    }
+}
