57e5a775693c41f24a376d47a1e42f447e691226
JHNUXER <johnhoganuke2@gmail.com>
2019-03-12 13:47:34 -0400

    Update: Modified shared layout (navbar) added other create method in ProjectController (not yet functional)

--- JXDevPlanner.WebMVC/Controllers/ProjectController.cs
+++ JXDevPlanner.WebMVC/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JXDevPlanner.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -17,6 +18,17 @@ namespace JXDevPlanner.WebMVC.Controllers
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ProjectCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
 
     }
