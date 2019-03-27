b2269b77c3b370c274bfe5bb336455c3147c3c5d
JHNUXER <johnhoganuke2@gmail.com>
2019-03-27 11:35:03 -0400

    Added TimeTool for changing DateTimeOffsets to nice neat text representation. Added PlanItemPromotion data entity. Modified Project Details view to make Date Created appear nicer.

---
+++ JXDevPlanner.Data/PlanItemPromotion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXDevPlanner.Data
+{
+    public class PlanItemPromotion
+    {
+        public Guid PlanId { get; set; }
+        public int OldCategory { get; set; }
+        public int NewCategory { get; set; }
+        public DateTimeOffset DateImplemented { get; set; }
+        public string Summary { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ JXDevPlanner.Storage/DateTimeTool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JXDevPlanner.Storage
+{
+    public class TimeTool
+    {
+        public static string Date(DateTimeOffset offs) => offs.ToString("MM/dd/yyyy");
+        public static string Date(DateTimeOffset? offs) => offs != null ? Date((DateTimeOffset)offs) : "N/A" ;
+        public static string DateTime(DateTimeOffset offs) => offs.ToString("MM/dd/yyyy @hh:mmtt");
+    }
+}
